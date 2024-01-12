5a0b5f8898ebf419f8719e014590a6da7bc999dc
Salvatore Assennato <137400679+Salvatore-12@users.noreply.github.com>
2024-01-12 11:47:27 +0100

    Initial project commit

---
+++ src/test/java/salvatoreassennato/u5w1d5/U5w1d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package salvatoreassennato.u5w1d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w1d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
